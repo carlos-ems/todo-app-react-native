fa03617f806052d3d2466b572f2e08a796876642
carlos <cmsilva1601@gmail.com>
2025-12-14 22:59:38 -0300

    Atualização do arquivo types.ts, adicionando os campos: id da lista de tarefas, nota e data de vencimento da tarefa.

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,17 @@
 export type uuid = string;
 
-export type TodoItem = { id: uuid; text: string; done: boolean, createdAt: Date };
+export type TodoItem = {
+    id: uuid;
+    text: string;
+    done: boolean,
+    createdAt: Date,
+
+    listId: uuid,
+    notes?: string,
+    dueDate: Date
+};
+
+export type TodoList = {
+    id: uuid,
+    name: string
+};
